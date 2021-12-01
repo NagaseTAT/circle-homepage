d8a345665e4d2f7a4f58ab93ee03b08289f6d61c
yudai <yuta3ru.rider@gmail.com>
2021-12-01 16:01:53 +0900

    12/1 ユーザー情報変更

--- accounts/views.py
+++ accounts/views.py
@@ -8,9 +8,14 @@ from django.contrib.auth import login
 from django.http import HttpResponseRedirect
 from django.urls import reverse_lazy
 from django.views.generic.edit import CreateView
+
+from blog.views import adminPostsList
 from .forms import SignUpForm, User
 from django.contrib.auth.decorators import login_required
 from    django.contrib.auth.decorators  import  login_required
+from django.shortcuts import render,redirect # redirectを追記
+from django.http import Http404
+
 
 
 # class SignUpView(generic.CreateView):
@@ -32,17 +37,16 @@ class SignUp(CreateView):
 @login_required
 def userEdit(request):
     user_id = request.user.id
-    old_user = User.objects.get(id = user_id)
+    try:
+        old_user = User.objects.get(id = user_id)
+    except User.DoesNotExist:
+        raise Http404
     if request.method == "POST":
-        old_user.title = request.POST["title"]
-        old_user.text = request.POST["text"]
-        date = datetime.datetime.now()
-        if old_user.username != '' & old_user.first_name != '' & old_user.last_name != '' & old_user.email != '':
-            old_user.save()
+        old_user.username = request.POST["username"]
+        old_user.first_name = request.POST["first_name"]
+        old_user.last_name = request.POST["last_name"]
+        old_user.email = request.POST["email"]
+        old_user.save()
         return redirect(adminPostsList)
-    else:
-        # placeholder設定のため以下２行
-        initial_dict = dict(old_user = username )
-        form = PostForm(request.GET or None, initial=initial_dict)
 
-        return render(request, 'blog/AdminPostsRegister.html', {})
+    return render(request, 'accounts/useredit.html', {"old_user":old_user})
